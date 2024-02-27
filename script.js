ecf89e4170cae41c88a444360e21072c3637bcea
Cecelia Braswell <ceceliabraswell@yahoo.com>
2024-02-26 22:05:18 -0800

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const toggleButton = document.getElementById("toggleButton");
+const navLinks = document.getElementById("navLinks");
+
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// send email function for contact form
 function sendEmail() {
-    alert("Email sent!");
-    document.getElementById('reset').reset();
+  alert("Email sent!");
+  document.getElementById("reset").reset();
 }
